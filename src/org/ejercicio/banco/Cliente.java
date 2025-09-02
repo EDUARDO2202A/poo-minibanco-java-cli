0273b29cc92999778fc885ad8c0761b2383c40e2
EDUARDO2202A <eduardocanche.mcc@gmail.com>
2025-09-02 03:11:41 +0000

    Actividad Poo: Minibanco inicial

---
+++ src/org/ejercicio/banco/Cliente.java
@@ -0,0 +1,12 @@
+package org.ejercicio.banco;
+
+class Cliente {
+    String nombre;
+    long dni;
+    Cliente (String str, long num) {
+
+        this.nombre = str;
+        this.dni = num;
+
+    }
+}
---
+++ src/org/ejercicio/banco/Main.java
@@ -0,0 +1,8 @@
+package org.ejercicio.banco;
+
+public class Main {
+    public static void main (String[] args){
+        Cliente cliente1 = new Cliente("Analicia",1170041);
+        CuentaBancaria cuenta1 = new CuentaBancaria(2872454,cliente1,10000);
+    }
+}
